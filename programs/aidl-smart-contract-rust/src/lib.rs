6e015a66a4c1bc7e185a5835241d93cdca01b9f7
skywalkerAlex <alexdim.liak@gmail.com>
2022-08-17 00:36:18 +0300

    Upload dataset details  Completed + unit tests

--- programs/aidl-smart-contract-rust/src/lib.rs
+++ programs/aidl-smart-contract-rust/src/lib.rs
@@ -1,15 +1,80 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AxEL6oqhuMeeGnAZJ9rzoM9FztdBUt3t7LmKgUwKF3Dk");
 
 #[program]
 pub mod aidl_smart_contract_rust {
     use super::*;
 
+    
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_datasets = 0;
         Ok(())
     }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn upload_dataset_details(ctx: Context<UploadDatasetDetails>, uploaded_dataset: DatasetObj) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user; 
+
+        let dataset_item = DatasetObj {
+            user_address: *user.to_account_info().key,
+            name: uploaded_dataset.name,
+            size: uploaded_dataset.size,
+            accuracy_score: uploaded_dataset.accuracy_score,
+            data_type: uploaded_dataset.data_type,
+            file_type: uploaded_dataset.file_type,
+            models_used: uploaded_dataset.models_used,
+            libraries_used: uploaded_dataset.libraries_used,
+        };
+        // Add it to the account vector.
+        base_account.datasets_list.push(dataset_item);
+        base_account.total_datasets += 1;
+        Ok(())
+    }
+
 }
 
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct UploadDatasetDetails<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Attach certain variables to the Initialize context.
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)] // 'space' in bytes
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_datasets: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub datasets_list: Vec<DatasetObj>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct DatasetObj {
+    pub user_address: Pubkey,
+    pub name: String,
+    pub size: String,
+    pub accuracy_score: String,
+    pub data_type: String,
+    pub file_type: String,
+    pub models_used: Vec<String>,
+    pub libraries_used: Vec<String>,
+}
--- tests/aidl-smart-contract-rust.js
+++ tests/aidl-smart-contract-rust.js
@@ -1,13 +1,63 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe("aidl-smart-contract-rust", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.AidlSmartContractRust;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.AidlSmartContractRust;
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  console.log("📝 First method initialize");
+
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Total Datasets Count', account.totalDatasets.toString())
+
+  let inputValue =  {
+    name: "name",
+    dataType: "dataType",
+    accuracyScore: "accuracyScore",
+    fileType: "fileType",
+    size: "fileSize",
+    modelsUsed: ["modelList"],
+    librariesUsed: ["libraryList"],
+  }
+  await program.rpc.uploadDatasetDetails(inputValue, {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+    });
+
+     // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Total Datasets Count', account.totalDatasets.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 Datasets List', account.datasetsList)
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
